05a12d6bd11792c039bf3c7a92b33e7d32f3d3f0
RacsonTech <58871762+RacsonTech@users.noreply.github.com>
2022-11-25 16:05:05 -0500

    Update ledblink.c

--- PCB/OnBoard_LED_blink/ledblink.c
+++ PCB/OnBoard_LED_blink/ledblink.c
@@ -14,43 +14,22 @@
  */
 int main(int argc, char** argv)
 {
-    uint32_t *led_ptr;
+//    uint32_t *led_ptr;
     
-    // disable watchdog timer
-//    WDT_REGS->WDT_MR
+    // Disable watchdog timer
+    WDT_REGS->WDT_MR = WDT_MR_WDDIS(1);
 
-//    led_ptr = (uint32_t *)0x400E1470;
-////    0x400E1470
-//    *led_ptr = 0x8;
-
-    // Is this not necessary?
-//    PIOA_REGS->PIO_WPMR &= ~0x1;    // clear bit 0 of write protection (allows write to pio en)
-
-//    PIOA_REGS->PIO_PER |= PIN_PA3;  // enable desired pin
-//    PIOA_REGS->PIO_OER |= PIN_PA3;  // output enable desired pin
-//    PIOA_REGS->PIO_OWER |= PIN_PA3; // output write enable desired pin
-    
-//    PIOA_REGS->PIO_PER |= PIO_PA3;  // enable desired pin
-//    PIOA_REGS->PIO_OER |= PIO_PA3;  // output enable desired pin
-//    PIOA_REGS->PIO_OWER |= PIO_PA3; // output write enable desired pin
-    
     
     PIOA_REGS->PIO_PER |= PIO_PA22;  // enable desired pin
     PIOA_REGS->PIO_OER |= PIO_PA22;  // output enable desired pin
     PIOA_REGS->PIO_OWER |= PIO_PA22; // output write enable desired pin
     
-//    PIOA_REGS->PIO_OWDR |= ~0x0; // output write disable desired pin
 
-
-    
     for (;;)
     {
-//        PIOA_REGS->PIO_ODSR ^= PIN_PA3;
         PIOA_REGS->PIO_ODSR ^= PIO_PA22; // PIO_ODSR = Output Data Status Register
 
-
-        for (volatile uint32_t i = 0; i < 200000; i++);
-//        *led_ptr ^= 0x8;
+        for (volatile uint32_t i = 0; i < 500000; i++);
     }
     
     return (EXIT_SUCCESS);
